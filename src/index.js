a562d59506b18c1a1b6db49ea29e40268a2dd54d
Jack Baron <jackmbaron@gmail.com>
2018-07-07 07:10:13 +0100

    refactor stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './css/index.css'
+import App from './js/App.jsx'
+import registerServiceWorker from './js/worker/registerServiceWorker.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
