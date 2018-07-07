d13b518914bb87c5f7d37aa559f5bafecef9a76e
Jack Baron <jackmbaron@gmail.com>
2018-07-07 08:30:42 +0100

    Use Bulma for main page
    wow look a nice UI

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
-import '../css/App.css'
+import 'bulma/css/bulma.css'
 
+import Tooltip from './Tooltip.jsx'
 import UI from './UI.jsx'
 import checkWS from './helpers/checkWS.js'
+import noise from '../images/noise.png'
 
 class App extends Component {
   constructor (props) {
@@ -37,17 +39,53 @@ class App extends Component {
   render () {
     if (!this.state.ws) {
       return (
-        <div>
-          <input
-            type='text'
-            placeholder='ws://localhost:3702'
-            value={ this.state.input }
-            onChange={ e => { this.setState({ input: e.target.value }) }}
-            onKeyDown={ e => { if (e.key === 'Enter') { this.checkURL() } } }
-          />
-          <button onClick={ () => this.checkURL() }>Connect to Server</button>
-          <div>{ this.state.error ? 'WebSocket Server Not Found' : '' }</div>
-        </div>
+        <section className='hero is-dark is-fullheight' style={{
+          backgroundImage: `url(${noise})`,
+          backgroundColor: 'hsl(0, 0%, 11%)',
+          overflow: 'hidden',
+        }}>
+          <div className='hero-body'>
+            <div className='container has-text-centered'>
+              <h1 className='title'>Beat Saber Multiplayer Scores</h1>
+              <h2 className='subtitle'><a href='' target='_blank' rel='noopener noreferrer'>HOW TO USE</a></h2>
+
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <div className='field has-addons is-fullwidth'>
+                  <div className='control' style={{ maxWidth: '550px', width: '56vw' }}>
+                    <input
+                      className='input'
+                      type='text'
+                      placeholder='ws://localhost:3702'
+                      value={ this.state.input }
+                      onChange={ e => { this.setState({ input: e.target.value, error: false }) }}
+                      onKeyDown={ e => { if (e.key === 'Enter') { this.checkURL() } } }
+                    />
+                  </div>
+                  <div className='control'>
+                    <a className={ `button ${this.state.error ? 'is-danger' : 'is-info'}` } onClick={() => { this.checkURL() }}>
+                      CONNECT
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <Tooltip
+                text={ this.state.error ? 'WebSocket Server Not Found...' : '' }
+                success={ false }
+                error={ this.state.error }
+              />
+            </div>
+          </div>
+
+          <div className='hero-foot'>
+            <div className='foot has-text-centered'>
+              <p>
+                Built by <strong>lolPants#0001</strong>
+              </p>
+            </div>
+          </div>
+
+        </section>
       )
     } else {
       return <UI ws={ this.state.ws } />
---
+++ src/js/Tooltip.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ToolTip = props => {
+  let isBlank = props.text === ''
+  let style = props.success ? 'is-success' : props.error ? 'is-danger' : ''
+
+  return <p className={ `help ${isBlank ? 'is-invis' : ''} ${style}` }>{ isBlank ? '-' : props.text }</p>
+}
+
+ToolTip.propTypes = {
+  text: PropTypes.string,
+  success: PropTypes.bool,
+  error: PropTypes.bool,
+}
+
+export default ToolTip
