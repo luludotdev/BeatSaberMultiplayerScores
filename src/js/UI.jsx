7666d0e1537b70f460778748eee7521be5aaa925
Jack Baron <jackmbaron@gmail.com>
2018-07-07 07:43:52 +0100

    Add PropTypes

--- src/js/UI.jsx
+++ src/js/UI.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class UI extends Component {
+  static propTypes = {
+    ws: PropTypes.any.isRequired,
+  }
+
   render () {
     return (
       <div>
