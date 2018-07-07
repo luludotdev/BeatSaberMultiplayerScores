76052968e0920314f02a98a8722304ef0fe27d58
Jack Baron <jackmbaron@gmail.com>
2018-07-07 10:31:38 +0100

    fuck

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './css/index.css'
 import App from './js/App.jsx'
-import registerServiceWorker from './js/worker/registerServiceWorker.js'
+// import registerServiceWorker from './js/worker/registerServiceWorker.js'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+// registerServiceWorker()
